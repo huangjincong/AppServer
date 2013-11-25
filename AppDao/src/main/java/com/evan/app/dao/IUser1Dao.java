df7ece39ef30694f19c4a247bd1ca4117fe78bc0
huangjincong <huangjincong@foxmail.com.cnm>
2013-11-25 23:11:43 +0800

    Dao Interfaces
    This is Dao Module which will be invoked by the Service Impl Module.

---
+++ AppDao/src/main/java/com/evan/app/dao/IUser1Dao.java
@@ -0,0 +1,7 @@
+package com.evan.app.dao;
+
+import com.evan1.app.po.User1;
+
+public interface IUser1Dao {
+	public User1 insertUser1(User1 user);
+}
---
+++ AppDao/src/main/java/com/evan/app/dao/IUser2Dao.java
@@ -0,0 +1,7 @@
+package com.evan.app.dao;
+
+import com.evan2.app.po.User2;
+
+public interface IUser2Dao {
+	public User2 insertUser2(User2 user);
+}
